aa716d6f2673a77e4790d622e7db2fb75237d310
DavidYannick <57354908+davidyannick86@users.noreply.github.com>
2025-01-13 19:58:15 +0100

    Add are_both_true function and corresponding test

--- src/main.rs
+++ src/main.rs
@@ -6,12 +6,28 @@ fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
     }
 }
 
+fn are_both_true<T, U, V>(f1: T, f2: U, number: &V) -> bool
+where
+    T: Fn(&V) -> bool,
+    U: Fn(&V) -> bool,
+{
+    f1(number) && f2(number)
+}
+
 fn main() {
     let string1 = String::from("abcd");
     let string2 = "xyz";
 
     let result = longest(string1.as_str(), string2);
     println!("The longest string is {}", result);
+
+    let expected = &4;
+
+    let add = |x: &i32| *x + *x == *expected;
+    let square = |x: &i32| *x * *x == *expected;
+
+    let result = are_both_true(add, square, &2);
+    println!("Are both true : {}", result);
 }
 
 #[cfg(test)]
@@ -34,5 +50,16 @@ mod tests {
 
         let result = longest(string1.as_str(), string2);
         assert_ne!(result, "xyz");
+    }
+
+    #[test]
+    fn test_are_both_true() {
+        let expected = &4;
+
+        let add = |x: &i32| *x + *x == *expected;
+        let square = |x: &i32| *x * *x == *expected;
+
+        let result = are_both_true(add, square, &2);
+        assert_eq!(result, true);
     }
 }
